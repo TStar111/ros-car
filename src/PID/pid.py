6f59dc081f0f2d5b3daaea466360074dc6906de5
Dante Biase <dab220@lehigh.edu>
2023-07-20 14:03:25 -0400

    Initial implementation of PID

---
+++ src/PID/pid.py
@@ -0,0 +1,91 @@
+class Follower:
+	def __init__(self, P, I, D, rate):
+		self.P = P
+		self.I = I
+		self.D = D
+		self.rate = rate
+		self.integral_prior = 0
+		self.error_prior = 0
+
+	def control(self, target, current):
+		"""
+		Parameters
+		----------
+		target 	- float - represents the x value of the goal location
+		current - float	- represents the x value of the current location
+
+		Returns
+		-------
+		theta - float - represents what the steering angle should be
+		speed - float - represents what the speed should be
+		"""
+		# Calculate the new desired angle
+		error = target - current
+		integral = self.integral_prior + error * self.rate
+		derivative = (error - self.error_prior) / self.rate
+		theta = (self.P * error) + (self.D * derivative) + (self.I * integral)
+
+		# Adjust integral and derivative values
+		self.error_prior = error
+		self.integral_prior = integral
+
+		# TODO: Firgure out how to manage speed with depth input
+		return theta, 0.5
+
+
+# Previous example of PID control
+
+# class robot_follower:
+#     def __init__(self):
+#         drive_topic = '/vesc/high_level/ackermann_cmd_mux/input/nav_0'
+#         drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped, queue_size=10)
+
+#         listener = tf.TransformListener()
+# 		trans = []
+# 		rot = []
+# 		integral_prior = 0
+# 		error_prior = 0
+# 		kp = 0.5
+# 		kd = 0.05
+# 		ki = 0
+# 		rate = 0.01
+#         while not rospy.is_shutdown():
+# 			try:
+# 				(trans, rot) = listener.lookupTransform('/tag_0', 'camera_color_optical_frame', rospy.Time(0))
+# 			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+# 				continue
+# 			print(trans)
+# 			x = float("{0:.2f}".format(trans[0]))
+# 			angle  = rot[2]
+# 			error  = 0 - angle
+# 			integral = integral_prior + error *rate
+# 			derivative = (error - error_prior)/rate
+# 			theta = kp*error + kd*derivative + ki*integral
+# 			error_prior = error
+# 			integral_prior = integral
+
+# 			if trans[2] >= 0.4:
+# 				#curv = ((2*x)/(x*x+z*z))
+# 				velocity = 0.5
+# 				theta  = theta
+				
+# 			else:
+# 				velocity = 0
+# 				theta = 0
+# 			print(theta)
+# 			drive_msg = AckermannDriveStamped()
+# 			drive_msg.header.stamp = rospy.Time.now()
+# 			drive_msg.header.frame_id = "camera_color_optical_frame"
+# 			drive_msg.drive.steering_angle = theta
+# 			drive_msg.drive.speed = velocity
+# 			drive_pub.publish(drive_msg)
+
+
+    
+
+# def main(args):
+#     rospy.init_node('robot_follower')
+#     rf = robot_follower()
+#     rospy.spin()
+# if __name__ == '__main__':
+#     main(sys.argv)
--- src/cv_bridge_alt/cv_bridge.py
+++ src/cv_bridge_alt/cv_bridge.py
@@ -3,6 +3,8 @@
     CV_Bridge, the module usually responsible for doing this, is not compatible with Python 3,
      - the language this all is written in.  So we create this module, and all is... well, all is not well,
      - but all works.  :-/
+
+     DEPRECATED FOR OUR PURPOSES
 """
 import sys
 import numpy as np
@@ -10,7 +12,7 @@ import cv2 as cv
 import rospy
 from sensor_msgs.msg import Image
 
-def imgmsg_to_cv2(img_msg):
+def bgraimgmsg_to_cv2(img_msg):
 
     # TODO: How do we handle conversion of bgra8 format from camera to rgb for YOLOP
     if img_msg.encoding != "bgra8":
@@ -34,6 +36,32 @@ def imgmsg_to_cv2(img_msg):
 
     # Make a writeable copy
     copy_image = np.copy(image_opencv)
+    return copy_image
+
+def depthimgmsg_to_cv2(img_msg):
+
+    # TODO: How do we handle conversion of bgra8 format from camera to rgb for YOLOP
+    if img_msg.encoding != "32FC1":
+        rospy.logerr("This Coral detect node has been hardcoded to the 'bgra8' encoding.  Come change the code if you're actually trying to implement a new camera")
+    dtype = np.dtype("float") # Hardcode to 8 bits...
+    dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
+    image_opencv_depth = np.ndarray(
+        shape=(img_msg.height, img_msg.width), # 1 Channel
+        dtype=dtype,
+        buffer=img_msg.data
+    )
+
+    # TODO: I don't think I need to do this?
+    # This handles conversion from BGRA to RGB
+    # image_opencv = image_opencv_bgra[...,:3][...,::-1]
+    # image_opencv = cv.cvtColor(image_opencv_bgra, cv.COLOR_BGRA2RGB)
+
+    # If the byt order is different between the message and the system.
+    if img_msg.is_bigendian == (sys.byteorder == 'little'):
+        image_opencv = image_opencv.byteswap().newbyteorder()
+
+    # Make a writeable copy
+    copy_image = np.copy(image_opencv_depth)
     return copy_image
 
 # This isn't checked since I don't plan on using it
